998b1f6ac3e5c13e5e23e069000a62d0fa055388
FatemeMokhtari <fatemeh4552@gmail.com>
2023-11-23 20:40:39 +0330

    Add Qr Code File and QrCode Ui File.

---
+++ QrCode.py
@@ -0,0 +1,64 @@
+import qrcode
+import sys
+from tkinter import filedialog
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide6.QtWidgets import *
+
+## ==> SPLASH SCREEN
+from Ui.Ui_QrCode import Ui_MainWindow
+
+
+class QrCode(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+         ## REMOVE TITLE BAR
+        #self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+            
+        def location():            
+            filepathgif, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Save file", "", "Image files (*.png)")
+            if len(filepathgif) > 0:                
+                generate(pathh=filepathgif)
+            
+        def generate(pathh):
+            try:
+                img = qrcode.make(self.ui.txtUrl.text())
+                type(img)
+                img.save(pathh)
+                
+                QtCore.QTimer.singleShot(1000, lambda:self.ui.label_2.setVisible(True))
+                QtCore.QTimer.singleShot(4000, lambda:self.ui.label_2.setVisible(False))
+            except:
+                print("error")
+
+        def show():
+            img = qrcode.make(self.ui.txtUrl.text())
+            type(img)
+            img.show() 
+        
+        def close():  
+                  
+            sys.exit(app.exec())
+            
+            
+        self.ui.ExitButton.clicked.connect(close)    
+        self.ui.btnshow.clicked.connect(show)  
+        self.ui.savebtn.clicked.connect(location) 
+         
+        self.ui.label_2.setVisible(False)
+        self.show()
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QrCode()
+    sys.exit(app.exec())
+
+
+
+
