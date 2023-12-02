0639b6bd42db33df21aea601600d9d11740f5c5f
FatemeMokhtari <fatemeh4552@gmail.com>
2023-12-02 18:11:42 +0330

    Change width window

--- AboutApp.py
+++ AboutApp.py
@@ -1,22 +1,13 @@
-from moviepy.editor import VideoFileClip
-
-from tkinter import filedialog , messagebox
 import sys
-import os
-import platform
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide6.QtWidgets import *
-from PySide6.QtGui import QMovie
 
-## ==> SPLASH SCREEN
+
 from Ui.Ui_AboutApp import Ui_MainWindow
 
 
-
-    
-# SPLASH SCREEN
 class About(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
@@ -24,25 +15,16 @@ class About(QMainWindow):
         self.ui.setupUi(self)
         
         
-        
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         
-        
-        
-        
-        def close():  
-              
+        def close():              
             sys.exit(app.exec())
         
         self.ui.ExitButton.clicked.connect(close)
         self.show()
         
-        
-        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = About()
-    sys.exit(app.exec())       
-        
-    
+    sys.exit(app.exec())
