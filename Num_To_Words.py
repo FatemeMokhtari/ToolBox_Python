c7a2b1cebf41427905bd06a2cf958b3cb54d5671
FatemeMokhtari <fatemeh4552@gmail.com>
2023-12-02 03:45:36 +0330

    Add QRegularExpression For LineEdit

--- Num_To_Words.py
+++ Num_To_Words.py
@@ -1,32 +1,26 @@
 import sys
-import os
-import platform
+
 from PySide6 import QtCore, QtGui, QtWidgets
-from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent, QRegularExpression as QRegExp)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient, QRegularExpressionValidator as QRegExpValidator)
 from PySide6.QtWidgets import *
-from googletrans import Translator,LANGUAGES
-import speech_recognition as sr
-from translate import Translator as translatevoice
+
 from num2words import num2words
 
-
-## ==> Num to Word
 from Ui.Ui_Num_To_Words import Ui_MainWindow
 
-
-
-    
 # Num to Word
 class NumToWords(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-        def numberclose():  
-            #QApplication.quit()   
-            #window.destroy()       
+        
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+        
+        def numberclose():        
             sys.exit(app.exec())
             
             
@@ -48,13 +42,15 @@ class NumToWords(QMainWindow):
         
                 
         
-        self.ui.lineEdit.textChanged.connect(timertxt)
+        self.ui.lineEdit.textChanged.connect(timertxt)        
         
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.ui.label.setWordWrap(True)
-        
+       
+        regex = QRegExp("^[ 0-9]+$")
+        validator = QRegExpValidator(regex, self.ui.lineEdit)        
+        self.ui.lineEdit.setValidator(validator)
         self.ui.lineEdit.setMaxLength(18)
+        
         self.show()
         
 if __name__ == "__main__":
